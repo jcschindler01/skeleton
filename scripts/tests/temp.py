a404347700faa4a55c9d5ae6ed19741c96946c89
jcschindler01 <jcschindler01@gmail.com>
2022-03-13 12:49:32 -0700

    Added a toggle to import from system or locally in test script.

--- scripts/tests/temp.py
+++ scripts/tests/temp.py
@@ -1,8 +1,15 @@
 
+## Run locally if True
+if False:
+  import sys
+  sys.path.insert(0,'G:\\My Drive\\physics\\2022\\skeleton\\src')
 
+## import
 import skeleton
 
-print(help(skeleton))
+
+## test
 
 skeleton.sub1.modA.hello()
+
 
