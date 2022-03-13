10940bfbd101a45f2489e8e91de739d7d8b83366
jcschindler01 <jcschindler01@gmail.com>
2022-03-13 14:28:56 -0700

    Revert to v0.0.4.

--- scripts/tests/temp.py
+++ scripts/tests/temp.py
@@ -1,15 +1,8 @@
 
-## Run locally if True
-if False:
-  import sys
-  sys.path.insert(0,'G:\\My Drive\\physics\\2022\\skeleton\\src')
 
-## import
 import skeleton
 
-
-## test
+print(help(skeleton))
 
 skeleton.sub1.modA.hello()
-
 
--- src/skeleton/__init__.py
+++ src/skeleton/__init__.py
@@ -1,6 +1,3 @@
 
 from skeleton import sub1
 from skeleton import sub2
-
-from . import _version
-__version__ = _version.get_versions()['version']
