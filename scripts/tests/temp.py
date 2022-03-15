bf45cfa0df5c772a85e3f96dc9aaf98add6d9740
jcschindler01 <jcschindler01@gmail.com>
2022-03-14 20:59:18 -0700

    Deleted local install flag... switching to pip install -e development mode.

--- scripts/tests/temp.py
+++ scripts/tests/temp.py
@@ -1,14 +1,4 @@
-## Run locally if True
-if True:
-  import sys
-  sys.path.insert(0,'G:\\My Drive\\physics\\2022\\skeleton\\src')
 
-## import
 import skeleton
 
-## test
-from importlib.metadata import version as md
-
-print(skeleton.__file__)
-
-print(dir(skeleton))
+help(skeleton)
