dd8e77e5a0e069449c365375ed18f8af03c2674e
jcschindler01 <jcschindler01@gmail.com>
2022-03-25 17:07:01 -0700

    Copied in some settings from qvis.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,8 @@ author = 'jcschindler01'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx.ext.autodoc',
+  'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +46,61 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Other Settings ----------------------------------------------------------
+
+# Logo
+html_logo = '_static/logo.png'
+
+# Change sidebar. Method from stackexchange (https://stackoverflow.com/questions/18969093/how-to-include-the-toctree-in-the-sidebar-of-each-page).
+html_sidebars = { '**': ['globaltoc.html', 'sourcelink.html', 'searchbox.html'] }
+
+
+
+
+
+
+
+
+# -- Copied in settings from jcschindler01/qvis
+
+
+# # Master doc
+# master_doc = 'index'
+
+# # Add any Sphinx extension module names here, as strings. They can be
+# # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# # ones.
+# extensions = [
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.napoleon',
+#    'sphinx.ext.mathjax',
+# ]
+
+# # autodoc settings
+# autodoc_member_order = 'groupwise'
+# autodoc_mock_imports = ['numpy','scipy','matplotlib']
+# autoclass_content = 'class'
+
+# # Napoleon settings
+# napoleon_google_docstring = False
+# napoleon_numpy_docstring = True
+# napoleon_include_init_with_doc = True
+# napoleon_include_private_with_doc = False
+# napoleon_include_special_with_doc = False
+# napoleon_use_admonition_for_examples = False
+# napoleon_use_admonition_for_notes = False
+# napoleon_use_admonition_for_references = False
+# napoleon_use_ivar = True
+# napoleon_use_param = True
+# napoleon_use_rtype = False
+
+
+
