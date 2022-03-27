61640544e0221c68441b288b000d1a637af5a56c
jcschindler01 <jcschindler01@gmail.com>
2022-03-26 21:15:10 -0700

    Try editing import directory.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-# print(os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../../src'))
+# print(os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
